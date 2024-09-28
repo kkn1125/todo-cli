fb963c49b6e77815e6913a2ef503fb141554795a
kkn1125 <xpfjrnseks1@naver.com>
2024-09-28 16:12:50 +0900

    🛠️ fix: 사용자명 및 저장소 동적으로 할당하도록 수정

--- src/util/pullRemoteDatabase.ts
+++ src/util/pullRemoteDatabase.ts
@@ -2,8 +2,13 @@ import { execSync } from "child_process";
 import path from "path";
 
 export function pullRemoteDatabase() {
+  const result = execSync('gh auth status | grep -ioe "account .*\\s"');
+  const repoInfo = execSync("gh repo view --json name");
+  const { name: repoName } = JSON.parse(repoInfo.toString("utf-8"));
+  const stringSet = result.toString("utf-8").trim().split(" ");
+  const { account } = Object.fromEntries([stringSet]);
   execSync(
-    `curl https://raw.githubusercontent.com/kkn1125/todo-cli/refs/heads/main/database/todo_list.json -o ${path.join(
+    `curl https://raw.githubusercontent.com/${account}/${repoName}/refs/heads/main/database/todo_list.json -o ${path.join(
       path.resolve(),
       "database/todo_list.json"
     )}`,
