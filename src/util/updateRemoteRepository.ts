abf7342ff78c24be9b635194859a0c46d69164a9
kkn1125 <xpfjrnseks1@naver.com>
2024-09-28 17:35:48 +0900

    🛠️ fix: gh api 구문 수정

--- src/util/updateRemoteRepository.ts
+++ src/util/updateRemoteRepository.ts
@@ -2,17 +2,7 @@ import { ACCOUNT, DATABASE_UPDATE_MESSAGE, REPO_NAME } from "@common/variables";
 import { execSync } from "child_process";
 
 export async function updateRemoteRepository() {
-  // await customSpawn("git", "add", path.join(DATABASE_DIR, DATABASE_NAME));
-  // await customSpawn("git", "commit", "-m", DATABASE_UPDATE_MESSAGE);
-  // await customSpawn("git", "push", "origin", "main");
   try {
-    // const result = execSync('gh auth status | grep -ioe "account .*\\s"');
-    // const repoInfo = execSync("gh repo view --json name");
-    // const { name: repoName } = JSON.parse(repoInfo.toString("utf-8"));
-    // const stringSet = result.toString("utf-8").trim().split(" ");
-    // // console.log("stringSet", stringSet);
-    // const { account } = Object.fromEntries([stringSet]);
-    // console.log(account, repoName);
     const base64 = execSync(`base64 -w 0 database/todo_list.json`);
     const sha = execSync(
       `gh api repos/${ACCOUNT}/${REPO_NAME}/contents/database/todo_list.json --jq .sha`,
@@ -21,7 +11,7 @@ export async function updateRemoteRepository() {
       }
     );
     execSync(
-      `gh api -X PUT -H "Content-Type: application/json" repos/${ACCOUNT}/${REPO_NAME}/contents/database/todo_list.json -F message="${DATABASE_UPDATE_MESSAGE}" -F content="${base64.toString(
+      `gh api -X PUT repos/${ACCOUNT}/${REPO_NAME}/contents/database/todo_list.json -F message="${DATABASE_UPDATE_MESSAGE}" -F content="${base64.toString(
         "utf-8"
       )}" -F sha="${sha.toString("utf-8")}"`,
       {
